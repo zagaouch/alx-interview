a2464c0fe080c89edb2ca3cc16f30d74aaf63d3a
zagaouch <i.zagaouch@gmail.com>
2024-05-09 23:10:12 +0100

    auto commit

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import math
+
+
+def minOperations(n) :
+    def gd(number):
+        greatest_divisor = 1
+        for i in range(2, number//2 + 1):
+            if number % i == 0:
+                greatest_divisor = i
+        return greatest_divisor
+
+    h = gd(n)
+    p = gd(n)
+    o = gd(n) + 1
+    while h < n :
+        h = h + p
+        o = o + 1
+    return o
