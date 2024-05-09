ed2a1e7227dca575f9ee9ee2318368adab109253
zagaouch <i.zagaouch@gmail.com>
2024-05-09 23:18:07 +0100

    auto commit

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,21 +3,19 @@
 Main file for testing
 """
 
-import math
-
 
 def minOperations(n):
-    def gd(number):
-        greatest_divisor = 1
-        for i in range(2, number//2 + 1):
-            if number % i == 0:
-                greatest_divisor = i
-        return greatest_divisor
+    if n <= 1:
+        return 0  # Base case: If n is 0 or 1, no operations needed
 
-    h = gd(n)
-    p = gd(n)
-    o = gd(n) + 1
-    while h < n:
-        h = h + p
-        o = o + 1
-    return o
+    operations = [0] * (n + 1)  # array to store the minimum operations
+    for i in range(2, n + 1):
+        operations[i] = float('inf')  # Initialize each value with infinity
+
+        # Try all possible factors j of i
+        for j in range(1, i):
+            if i % j == 0:
+
+                operations[i] = min(operations[i], operations[j] + (i // j))
+
+    return operations[n]  # Return the minimum operations for n
