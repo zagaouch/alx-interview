c57537b49852a1beb55b206e01c48839b91b2386
zagaouch <i.zagaouch@gmail.com>
2024-06-23 23:08:37 +0100

    tst1

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,17 +3,19 @@
 Main file for testing
 """
 
+
 def makeChange(coins, total):
     if total <= 0:
         return 0
-    
-    # Initialize dp array with a value greater than the possible number of coins needed
+
+    # Initialize dp array with a value greater than
+    # the possible number of coins needed
     dp = [total + 1] * (total + 1)
     dp[0] = 0
-    
+
     # Populate dp array
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
-    
+
     return dp[total] if dp[total] != total + 1 else -1
