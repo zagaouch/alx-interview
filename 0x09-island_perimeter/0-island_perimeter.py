b8de779860930f11bfdb528c2b60363870ba8068
zagaouch <i.zagaouch@gmail.com>
2024-06-25 18:27:31 +0100

    tst1

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+0. Island Perimeter
+"""
+
+def island_perimeter(grid):
+    if not grid or not grid[0]:
+        return 0
+
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                perimeter += 4
+                if r > 0 and grid[r-1][c] == 1:  # Check top neighbor
+                    perimeter -= 2
+                if c > 0 and grid[r][c-1] == 1:  # Check left neighbor
+                    perimeter -= 2
+
+    return perimeter
